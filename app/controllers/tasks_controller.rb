e4a6dbe6543569d7b052b10ea85c928160d3bc11
Abbas Nazarov <abbas.nazarov@gmail.com>
2022-11-15 17:18:20 +0400

    rails update1.0

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,39 @@
 class TasksController < ApplicationController
-  # before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
+  end
+
+  def new
+    @tasks = Task.new
+  end
+
+  def edit
+  end
+
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+
+  end
+
+  def update
+
+  end
+
+  def show
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-get "tasks", to: "tasks#index"
+# get "tasks", to: "tasks#index"
+# get "tasks/new", to: "tasks#new", as: :new_task
+# get "tasks/:id", to: "tasks#show", as: :task
+resources :tasks
 end
